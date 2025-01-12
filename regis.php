c448d9b607d896d24f58e0c0230728e5b464d34a
unknown <armeza07@gmail.com>
2025-01-12 22:22:59 +0800

    tambah registrasi php dan css

--- regis.php
+++ regis.php
@@ -1,19 +1,26 @@
-<?php
-        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isSet($_POST['sentForm'])) {
-                $conn = mysqli_connect('localhost', 'root', '','regis') or die("Connection failed: " . mysqli_connect_error());
-                if (isSet($_POST['user']) && isSet($_POST['email']) && isSet($_POST['pass'])) {
-                        $user = $_POST['user'];
-                        $pass = $_POST['pass'];
-                        $email = $_POST['email'];
- 
-                        $sql = "INSERT INTO `users` (`username`,`password`,`email`) VALUES ('$user', '$pass', '$email')";
- 
-                        $query = mysqli_query($conn,$sql);
-                        if ($query) {
-                                echo 'Data Successfullu Saved!';
-                        } else {
-                                echo "An error occured while save the data.";
-                        }
-                }
-        }
-?>  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Simpe Registration</title>
+        <link rel="stylesheet" href="regis.css">
+
+</head>
+<body align="middle">
+        <div><center><h2></h2></center></div>
+        <div class="container">
+        <form action='registration.php' method='POST'>
+                <h2>Formulir pendaftaran
+                </h2>
+                <label for="user"><h2>Username:</h2></label>     <br/>
+                <input type='text' name='user' id="user"  required/><br/>
+                <label for="user"><h2>Password:</h2></label>     <br/>
+                <input type='password' name='pass' id="pass"  required/><br/>
+                <label for="user"><h2>Email:</h2></label>        <br/>
+                <input type='email' name='email' id="email"  required/><br/>
+                <input type='submit' name='sentForm' id="sentForm" />
+        </form>
+        </div>
+</body>
+</html>
